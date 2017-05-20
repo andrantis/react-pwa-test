9deefe15cef4e1085ee8f9c5188e2a604349f634
Andres Antista <andresantista@gmail.com>
2017-05-20 13:39:27 -0700

    100% pwa with custom service worker

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -9,9 +9,11 @@
 // This link also includes instructions on opting out of this behavior.
 
 export default function register() {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  // if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      // const swUrl = `../build/service-worker.js`;
       navigator.serviceWorker
         .register(swUrl)
         .then(registration => {
